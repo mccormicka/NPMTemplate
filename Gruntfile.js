7cdf6751e0b4a045901c57775bffe6c65770c15a
Anthony McCormick <=>
2013-09-08 00:44:31 +0100

    Added test runners to grunt file

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,15 @@
 module.exports = function (grunt) {
     'use strict';
 
+    //Default task runs tests, jshint and watches for changes.
     grunt.registerTask('default',
         ['jasmine_node', 'jshint', 'watch']);
+
+    //Just run tests
+    grunt.registerTask('test', 'jasmine_node');
+
+    //Alias for default
+    grunt.registerTask('test:watch', 'default');
 
     // Project configuration.
     grunt.initConfig({
